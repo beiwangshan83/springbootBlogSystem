524008ccbaedf04b3ce71f78de940bbf3a1a1fdd
beiwangshan <1797007941@qq.com>
2020-11-17 18:53:21 +0800

    准备阶段

--- src/main/java/com/beiwangshan/blog/response/ResponseState.java
+++ src/main/java/com/beiwangshan/blog/response/ResponseState.java
@@ -8,10 +8,24 @@ package com.beiwangshan.blog.response;
  * @version: 1.0
  * @todo:
  */
-public class ResponseState {
+public enum  ResponseState {
+    /**
+     * 因为一般 2开头的 http状态为成功，为了区别，这里定义为20000
+     * 4开头的http状态为失败，所以这里定义为40000
+     */
+    SUCCESS(true,20000,"操作成功"),
+    LOGIN_SUCCESS(true,20001,"登录成功"),
+    FAILED(false,40000,"操作失败");
+
     private int code;
     private String message;
     private boolean success;
+
+    ResponseState(boolean success,int code,String message){
+        this.code=code;
+        this.success=success;
+        this.message=message;
+    }
 
     public int getCode() {
         return code;
